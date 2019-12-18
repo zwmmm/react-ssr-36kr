5f7fdeac9e32642d5df552e0e573300ae310b38a
zwmmm <23490029@qq.com>
2019-12-18 16:53:59 +0800

    Update render.js

--- server/render.js
+++ server/render.js
@@ -1,1 +1,24 @@
-const { renderToString } = require('react-dom/server');function templating(template) {    return props => template.replace(/<!--([\s\S]*?)-->/g, (_, key) => props[key.trim()]);}module.exports = async function(ctx, serverBundle, template) {    try {        const render = templating(template);        const jsx = await serverBundle(ctx);        const html = renderToString(jsx);        const body = render({            html,            store: `<script>window.__STORE__ = ${JSON.stringify(ctx.store.getState())}</script>`,        });        ctx.body = body;        ctx.type = 'text/html';    }    catch (err) {        console.error(err.message);        ctx.body = err.message;        ctx.type = 'text/html';    }}
+const { renderToString } = require('react-dom/server');
+
+function templating(template) {
+    return props => template.replace(/<!--([\s\S]*?)-->/g, (_, key) => props[key.trim()]);
+}
+
+module.exports = async function(ctx, serverBundle, template) {
+    try {
+        const render = templating(template);
+        const jsx = await serverBundle(ctx);
+        const html = renderToString(jsx);
+        const body = render({
+            html,
+            store: `<script>window.__STORE__ = ${JSON.stringify(ctx.store.getState())}</script>`,
+        });
+        ctx.body = body;
+        ctx.type = 'text/html';
+    }
+    catch (err) {
+        console.error(err.message);
+        ctx.body = err.message;
+        ctx.type = 'text/html';
+    }
+} 
